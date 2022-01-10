041b8daf176fd8eb90fc97604345baf840c89ae8
poojagunturu96 <pgunturu@middlebury.edu>
2022-01-10 14:49:03 -0600

    Added deploy script for #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,35 @@
+const gulp = require('gulp');
+const args = require('yargs').argv;
 const { createConfig } = require('@middlebury/gulp-config');
 
-module.exports = createConfig();
+const THEME_DIR = process.env.THEME_DIR || args.themeDir || '';
+
+const cmds = createConfig();
+
+const replaceImagePaths = () => {
+  const imagesDir = args.imagesDir || '/images/';
+  return gulp
+    .src('./dist/css/*.css')
+    .pipe(replace('/images/', imagesDir))
+    .pipe(gulp.dest('./dist/css'));
+};
+
+const deployDist = () => {
+  if(!THEME_DIR) {
+    return console.error('No `--themeDir` argument passed');
+  }
+
+  return gulp
+    .src(['./dist/css/main.css', './dist/js/bundle.js', './dist/images/*'], {
+      base: './dist'
+    })
+    .pipe(gulp.dest(THEME_DIR));
+}
+
+const deploy = gulp.series(replaceImagePaths, deployDist);
+
+module.exports = {
+  ...cmds,
+  deploy
+};
 
