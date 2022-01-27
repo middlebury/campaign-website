3f6edb0ae9b0d526f0c4319f1ebc79839c22fb51
poojagunturu96 <pgunturu@middlebury.edu>
2022-01-27 10:01:58 -0600

    Updated build script to procees typescript, added dependencies to support chart component for #9

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const gulp = require('gulp');
 const replace = require('gulp-replace');
 const args = require('yargs').argv;
@@ -5,7 +6,34 @@ const { createConfig } = require('@middlebury/gulp-config');
 
 const THEME_DIR = process.env.THEME_DIR || args.themeDir || '';
 
-const cmds = createConfig();
+const { cwd } = process;
+
+const DIST_DIR = 'dist';
+const SOURCE_DIR = 'src';
+
+const dist = (parts = '') => path.resolve(cwd(), DIST_DIR, parts);
+const src = (parts) => path.resolve(cwd(), SOURCE_DIR, parts);
+
+
+const copyDeps = () => {
+  // NOTE: Chart.bundle.min.js includes Momentjs but so far we are not using time axis
+  // http://www.chartjs.org/docs/latest/getting-started/installation.html#bundled-build
+  return gulp
+    .src('./node_modules/chart.js/dist/Chart.min.js')
+    .pipe(gulp.dest('./dist/js'));
+};
+
+const options = {
+  scripts: {
+    src: src('js/index.ts'),
+    watch: src('js/**/*'),
+    dest: dist('js/bundle.js')
+  },
+  beforeBuild: [copyDeps],
+  typescriptBuild: true
+};
+
+const cmds = createConfig(options);
 
 const replaceImagePaths = () => {
   const imagesDir = args.imagesDir || '/images/';
